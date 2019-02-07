98160ea19f6a9d9eb52dbe777a4a8b0c4001de66
Daniel McAllister <danny.mcallister@centre.edu>
2019-02-07 17:45:16 -0500

    small fix

--- tictoe.js
+++ tictoe.js
@@ -1,6 +1,6 @@
 
 var start = function(){
-  var t= document.getElementbyId("t1");
+  var t= document.getElementById("t1");
 
   for(var i=1;i<4;i++){
       var r= document.createElement("tr");
@@ -8,7 +8,11 @@ var start = function(){
       for(var j=0; j<3;j++){
         var tableElement= document.createElement("td");
         var picture = document.createElement("img");
-        picture.setAttribute(src="white_back.PNG");
+        picture.setAttribute("src","white_back.PNG");
+        picture.setAttribute("id","picture"+j.toString()+i.toString())
+        picture.setAttribute("onclick","handle()")
+        picture.setAttribute("width","200");
+        picture.setAttribute("height","200");
         tableElement.appendChild(picture);
         r.appendChild(tableElement);
       }
