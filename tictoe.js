e3621df6b8b2265760a2ae1635f46ea220494a77
Daniel McAllister <danny.mcallister@centre.edu>
2019-02-07 18:20:05 -0500

    Done?

--- tictoe.js
+++ tictoe.js
@@ -12,12 +12,26 @@ var start = function(){
         var picture = document.createElement("img");
         picture.setAttribute("src","white_back.PNG");
         picture.setAttribute("id",ID)
-        picture.setAttribute("onclick","document.getElementById("+ID+").setAttribute('src',x.png)");
+        var clk="document.getElementById("+ID+").setAttribute('src',x.png)";
+        picture.setAttribute("onclick",clk);
         picture.setAttribute("width","200");
         picture.setAttribute("height","200");
         tableElement.appendChild(picture);
         r.appendChild(tableElement);
       }
       t.appendChild(r);
+  }
+}
+
+var myMove = function(){
+  var notYet = false;
+  var strs= ["00","01","02","10","11","12","20","21","22"];
+  for(var i=0;i<9;i++){
+    var name = "picture"+strs[i];
+    var pic= document.getElementById(name);
+    if(pic.getAttribute('src')=="white_back.PNG"){
+      pic.setAttribute("src","o.jpg")
+      break;
+    }
   }
 }
