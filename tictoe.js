7e956c1f4cc209142797475d36fc0df3f1cf4239
Daniel McAllister <danny.mcallister@centre.edu>
2019-02-07 18:02:23 -0500

    Most of it

--- tictoe.js
+++ tictoe.js
@@ -1,16 +1,18 @@
 
 var start = function(){
+  document.getElementById("kill").removeAttribute(onclick);
   var t= document.getElementById("t1");
 
   for(var i=1;i<4;i++){
       var r= document.createElement("tr");
 
       for(var j=0; j<3;j++){
+        var ID="picture"+j.toString()+i.toString();
         var tableElement= document.createElement("td");
         var picture = document.createElement("img");
         picture.setAttribute("src","white_back.PNG");
-        picture.setAttribute("id","picture"+j.toString()+i.toString())
-        picture.setAttribute("onclick","handle()")
+        picture.setAttribute("id",ID)
+        picture.setAttribute("onclick","document.getElementById(ID).setAttribute('src',x.png)");
         picture.setAttribute("width","200");
         picture.setAttribute("height","200");
         tableElement.appendChild(picture);
